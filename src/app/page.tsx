394aa1e1e46e9fd69dc906a8870fe59361c33ef1
Sameer Fidai <sameer.fidai09@gmail.com>
2025-01-14 21:00:08 -0500

    add pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,56 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ol className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li>Under development...</li>
+        <ol className="list-inside text-lg text-center font-[family-name:var(--font-geist-mono)]">
+          <li>
+            <a
+              className="hover:underline"
+              href="https://github.com/sameerfidai"
+              target="_blank"
+            >
+              github
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:underline"
+              href="https://www.linkedin.com/in/sameerfidai/"
+              target="_blank"
+            >
+              linkedin
+            </a>
+          </li>
+          <li>
+            <a
+              className="hover:underline"
+              href="https://github.com/sameerfidai"
+              target="_blank"
+            >
+              resume
+            </a>
+          </li>
+          <li>
+            <Link href="/projects" className="hover:underline">
+              projects
+            </Link>
+          </li>
+          <li>
+            <Link href="/experience" className="hover:underline">
+              experience
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" className="hover:underline">
+              about
+            </Link>
+          </li>
         </ol>
       </main>
 
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
